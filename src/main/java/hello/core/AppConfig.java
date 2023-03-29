2872914e98cb927e2fb8294ed46ae534ac218351
RyuJungSik <xvbc123@gmail.com>
2023-03-30 00:29:39 +0900

    Feat : 스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,22 +7,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
-private static MemoryMemberRepository memberRepository() {
+@Bean
+public MemoryMemberRepository memberRepository() {
     return new MemoryMemberRepository();
 }
 
+@Bean
 public MemberService memberService() {
     return new MemberServiceImpl(memberRepository());
 }
 
+@Bean
 public OrderService orderService() {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
 }
 
-public DiscountPolicy discountPolicy(){
+@Bean
+public DiscountPolicy discountPolicy() {
 //    return new FixDiscountPolicy();
     return new RateDiscountPolicy();
 }
