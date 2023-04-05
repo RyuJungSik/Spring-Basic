6d809e40c9fb7cf426a3b30ab8176ca6be1ab7bb
RyuJungSik <xvbc123@gmail.com>
2023-04-05 21:47:43 +0900

    Feat : @Component, @AutoWired를 통한 컴포넌트 스캔과 의존관계 자동주입 추가

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -32,5 +32,13 @@ void configurationTest(){
     
 }
 
+@Test
+void configurationDeep(){
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AppConfig bean = ac.getBean(AppConfig.class);
+    
+    System.out.println("bean = " + bean.getClass());
+    
+}
 
 }
