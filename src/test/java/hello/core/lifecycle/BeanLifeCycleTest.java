b999d9d731a7006e86ebdfa12a692d1cf453f7dc
RyuJungSik <xvbc123@gmail.com>
2023-04-10 23:12:35 +0900

    Feat : 빈 생명주기 콜백시장

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+@Test
+public void lifeCycleTest() {
+    
+    ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+    NeworkClient client = ac.getBean(NeworkClient.class);
+    ac.close();
+}
+
+@Configuration
+static class LifeCycleConfig {
+    @Bean
+    public NeworkClient networkClient() {
+        NeworkClient neworkClient = new NeworkClient();
+        neworkClient.setUrl("http://naver.com");
+        return neworkClient;
+    }
+    
+}
+
+}
